7930dc5344bc42bbc03a09c0970e1c0c56de07e5
LFO <lucasfronortin@gmail.com>
2021-08-31 17:15:29 +0200

    Add Multipage Class

---
+++ MultiPage.py
@@ -0,0 +1,25 @@
+"""
+    USe to define a class to manage multiple page on streamlit
+"""
+
+# Import streamlit
+import streamlit as st
+
+# Class definition
+class MultiPage:
+
+    def __init__(self)-> None:
+        self.pages = []
+
+    def add_pages(self,title,func)-> None:
+        """ Class Mehod to Add pages to the project"""
+        self.pages.append({
+            'title':title,
+            'function':func
+        })
+
+    def run(self):
+        page = st.sidebar.selectbox('Page Navigation',self.pages,format_func=lambda page:page['title'])
+
+        # run app function
+        page['function']()
