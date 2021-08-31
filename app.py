5df3b868cb7d66da6c80b7cad9e3ab8c3c11b9d0
LFO <lucasfronortin@gmail.com>
2021-08-31 17:31:51 +0200

    Add data_upload

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
 import streamlit as st
 
+from MultiPage import MultiPage
+
+# Create instance
+app=MultiPage()
+
+# Title of page
+st.title('Financial Dashboard')
+
+# Add Pages
+
+
+# Run
+app.run()
---
+++ pages/data_upload.py
@@ -0,0 +1,27 @@
+import collections
+from numpy.core.defchararray import lower
+import streamlit as st
+import numpy as np
+import pandas as pd
+from pages import utils
+
+
+def app():
+    st.markdown("## Data Upload")
+
+    # Upload the dataset and save as csv
+    st.markdown("### Upload a csv file for analysis.")
+    st.write("\n")
+
+    # Code to read a single file
+    uploaded_file = st.file_uploader("Choose a file", type=['csv', 'xlsx'])
+    global data
+    if uploaded_file is not None:
+        try:
+            data = pd.read_csv(uploaded_file)
+        except Exception as e:
+            print(e)
+            data = pd.read_excel(uploaded_file)
+
+    ''' Load the data and save the columns with categories as a dataframe. 
+    This section also allows changes in the numerical and categorical columns. '''
